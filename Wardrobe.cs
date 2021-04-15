ab27e56e8922e0319cd8db836660670076cfcd69
David2000Sobak <58178462+David2000Sobak@users.noreply.github.com>
2021-04-15 23:12:53 +0300

    Update Wardrobe.cs

--- Wardrobe.cs
+++ Wardrobe.cs
@@ -8,6 +8,7 @@ namespace Project2
 {
     class Wardrobe
     {
+        //Вид хранения искомых данных во время работы программы
         public string CanId { get; set; }
         public string Number { get; set; }
         public string Type { get; set; }
